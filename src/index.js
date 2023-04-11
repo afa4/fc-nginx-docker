ed0c7066b95368b87e1d8c26981f1e38892f03a3
Arthur Frade de Araujo <arthur.frade@loft.com.br>
2023-04-11 15:09:31 +0000

    express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('hello world');
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.setHeader('content-type', 'text/html')
+  res.send('<h1>Full Cycle Rocks!</h1>')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
